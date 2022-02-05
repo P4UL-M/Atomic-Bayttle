f50ba17db3f84a5d4b0b5fd9978900fd1a84051b
P4UL-M <paul.mairesse@free.fr>
2022-02-05 19:11:45 +0100

    game main

--- game.py
+++ game.py
@@ -1,5 +1,7 @@
 import pygame
 import screeninfo
+import game_main
+import menu_main
 
 pygame.init()
 
@@ -10,9 +12,12 @@ class Game:
     def run():
         pygame.display.set_mode(Game.size,pygame.RESIZABLE)
         while Game.running:
+            # boucle to intercept only the quit event
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     Game.running = False
+                else:
+                    pygame.event.post(event)
             pygame.display.update()
         else:
             raise SystemExit
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from game_manager import Game as game
+from game import Game as game
 
-a = game()
 
 if __name__=="__main__":
     try:
-        a.run()
+        game.run()
     except SystemExit:
         print("bye !")
