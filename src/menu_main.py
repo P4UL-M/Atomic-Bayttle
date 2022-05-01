a00d68203cb491b258fb1ef19fd355cbba0f6b95
P4UL-M <paul.mairesse@free.fr>
2022-05-02 00:08:17 +0200

    useless print

--- src/menu_main.py
+++ src/menu_main.py
@@ -949,7 +949,6 @@ def setup_manager():
                 _button.start = 0
                 for sprite in credits_menu.sprites():
                     if _button.name != sprite.name and "palmer" not in sprite.name:
-                        print(sprite.name)
                         sprite.set_position(sprite.base_position)
 
         @_button.on_click(PATH / "assets" / "sound" / "button-menu.wav")
